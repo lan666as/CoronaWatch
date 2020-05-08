dd6f272aa248313c3b152dc60938a79c5f703946
aldoarya00 <aldoarya00@mail.ugm.ac.id>
2020-05-08 08:40:21 +0700

    Added basic Viewmodel, testing windows, and some functionalities in JHUDataService

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -7,6 +7,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using CoronaWatchLibrary;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using Force.Crc32;
+using System.Text.RegularExpressions;
 
 namespace Testing
 {
@@ -14,7 +19,8 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("https://api.covid19api.com/" + "summary");
+            #region Testing Zidan
+            /*WebRequest request = WebRequest.Create("https://api.covid19api.com/" + "summary");
             request.Credentials = CredentialCache.DefaultCredentials;
             WebResponse response = request.GetResponse();
             Console.WriteLine(((HttpWebResponse)response).StatusDescription);
@@ -29,7 +35,29 @@ namespace Testing
             Console.WriteLine(Global.ToString());
 
 
-            Console.ReadKey();
+            Console.ReadKey();*/
+            #endregion
+
+            #region Testing Aldo
+
+            List<Region> regions = new List<Region>();
+            List<Report> reports = new List<Report>();
+
+            regions = JHUDataService.FetchAllRegion();
+            reports = JHUDataService.FetchRegionSummary();
+
+            foreach(Report report in reports)
+            {
+                Console.WriteLine(report.Statistic.StatisticID);
+                Region currentRegion = regions.Where(u => u.ISOCode == report.Statistic.StatisticID).FirstOrDefault();
+                Console.WriteLine(currentRegion.Name);
+                Console.WriteLine(report.Statistic.ConfirmedCases);
+                Console.WriteLine(report.LastUpdate);
+                Console.WriteLine("---------------------");
+            }
+
+            Console.Read();
+            #endregion
         }
 
         private void ParseWorldReports(JArray jArray, ref Region world)
