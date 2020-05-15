ab970986d1ff2e843f191e6adfa03332fb82cb51
lan666as <6338730+lan666as@users.noreply.github.com>
2020-05-16 00:02:00 +0700

    fix merge issues

--- CoronaWatchLibrary/Models/Statistic.cs
+++ CoronaWatchLibrary/Models/Statistic.cs
@@ -24,5 +24,12 @@
             this.DeathCases = death;
             this.ActiveCases = this.ConfirmedCases - this.DeathCases - this.RecoveredCases;
         }
+        public Statistic(int confirmed, int recovered, int death, int active)
+        {
+            this.ConfirmedCases = confirmed;
+            this.RecoveredCases = recovered;
+            this.DeathCases = death;
+            this.ActiveCases = active;
+        }
     }
 }
